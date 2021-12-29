569a3af71fd9017ccdf7b146b8f7415cc2f237c5
KrotosBenjamin <kj.benjamin90@gmail.com>
2021-12-29 16:04:15 -0500

    updating scripts for mdd analysis, adding SGD

--- mdd_analysis/brainseq/_h/sgd_rfe.py
+++ mdd_analysis/brainseq/_h/sgd_rfe.py
@@ -34,6 +34,22 @@ def mkdir_p(directory):
             raise
 
 
+def load_data(fn):
+    pandas2ri.activate()
+    globalenv['fn'] = fn
+    r('''
+    suppressMessages({library(tidyverse)})
+    load(fn)
+    expr = as.data.frame(v$E)
+    model = data.frame(v$design)
+    genes = rownames(expr)
+    weights = as.data.frame(v$weights) %>% mutate(rowname=rownames(v$E)) %>%
+      column_to_rownames(var="rowname")
+    colnames(weights) = colnames(v$E)
+    ''')
+    return r['expr'].T, r['model'].MDD.astype("int64")
+
+
 def extract_feature_annotation(pred_feat, path, fold, gtf_file):
     # Get important features
     dft = pd.DataFrame.from_records(pred_feat,
@@ -100,7 +116,7 @@ def residualize(train_index, test_index):
     return r['X_train'], r['X_test']
 
 
-def optimize_rf(X, Y, estimator, cv, outdir):
+def optimize_dev(X, Y, estimator, cv, outdir):
     fold_num = 0
     for train_index, test_index in cv.split(X, Y):
         Y_train, Y_test = Y[train_index], Y[test_index]
@@ -108,9 +124,9 @@ def optimize_rf(X, Y, estimator, cv, outdir):
     fold_num -= 1
     X_train, X_test = residualize(train_index, test_index)
     features = X_train.columns
-    d, pfirst = dRFEtools.rf_rfe(estimator, X_train.values, Y_train,
-                                 features, fold_num, outdir,
-                                 elimination_rate=0.1, RANK=False)
+    d, pfirst = dRFEtools.dev_rfe(estimator, X_train.values, Y_train,
+                                  features, fold_num, outdir,
+                                  elimination_rate=0.1, RANK=False)
     for frac in [0.2, 0.25, 0.3, 0.35]:
         plt.clf()
         dRFEtools.optimize_lowess_plot(d, fold_num, outdir, frac=frac,
@@ -124,51 +140,48 @@ def optimize_rf(X, Y, estimator, cv, outdir):
                                        save_plot=True)
 
 
-def rf_run(X_train, X_test, Y_train, Y_test, fold_num, outdir,
-           gtf_file, estimator, frac, step_size):
-    # Apply random forest
-    features = X_train.columns
-    d, pfirst = dRFEtools.rf_rfe(estimator, X_train.values, Y_train.values,
-                                 features, fold_num, outdir,
-                                 elimination_rate=0.1)
-    df_elim = pd.DataFrame([{'fold':fold_num, 'n features':k,
+def dev_run(x_train, x_test, y_train, y_test, fold, outdir,
+            gtf_file, estimator, frac, step):
+    features = x_train.columns
+    d, pfirst = dRFEtools.dev_rfe(estimator, x_train.values, y_train.values,
+                                  features, fold, outdir, elimination_rate=0.1)
+    df_elim = pd.DataFrame([{'fold':fold, 'n features':k,
                              'NMI score':d[k][1], 'Accuracy score':d[k][2],
                              'ROC AUC score':d[k][3]} for k in d.keys()])
-    # dRFEtools.plot_nmi(d, fold_num, outdir)
-    # dRFEtools.plot_acc(d, fold_num, outdir)
-    # dRFEtools.plot_roc(d, fold_num, outdir)
     n_features_max = max(d, key=lambda x: d[x][1])
     try:
-        # Max features based on lowess curve
-        n_features,_ = dRFEtools.extract_max_lowess(d, frac=frac)
-        n_redundant,_ = dRFEtools.extract_redundant_lowess(d, frac=frac,
-                                                           step_size=step_size)
-        dRFEtools.plot_with_lowess_vline(d, fold_num, outdir, frac=frac,
-                                         step_size=step_size, multi=False,
+        ## Max features from lowess curve
+        ### multiple classification is False by default
+        n_features, _ = dRFEtools.extract_max_lowess(d, frac=frac)
+        n_redundant, _ = dRFEtools.extract_redundant_lowess(d, frac=frac,
+                                                            step_size=step)
+        dRFEtools.plot_with_lowess_vline(d, fold, outdir, frac=frac,
+                                         step_size=step, multi=False,
                                          classify=True)
     except ValueError:
+        ## For errors in lowess estimate
         n_features = n_features_max
         n_redundant = n_features
-    # Fit model
-    estimator.fit(X_train, Y_train)
-    all_fts = estimator.predict(X_test)
-    estimator.fit(X_train.values[:, d[n_redundant][4]], Y_train)
-    labels_pred_redundant = estimator.predict(X_test.values[:, d[n_redundant][4]])
-    estimator.fit(X_train.values[:, d[n_features][4]], Y_train)
-    labels_pred = estimator.predict(X_test.values[:, d[n_features][4]])
-    # Output test predictions
+    ## Fit model
+    estimator.fit(x_train, y_train)
+    all_fts = estimator.predict(x_test)
+    estimator.fit(x_train.values[:, d[n_redundant][4]], y_train)
+    labels_pred_redundant = estimator.predict(x_test.values[:, d[n_redundant][4]])
+    estimator.fit(x_train.values[:,d[n_features][4]], y_train)
+    labels_pred = estimator.predict(x_test.values[:, d[n_features][4]])
+    ## Output test predictions
     kwargs = {"average": "weighted"}
-    pd.DataFrame({'fold': fold_num, 'real': Y_test,
+    pd.DataFrame({'fold': fold, 'real': y_test,
                   'predict_all': all_fts, 'predict_max': labels_pred,
                   'predict_redundant': labels_pred_redundant},
-                 index=X_test.index)\
-      .to_csv('%s/test_predictions.txt' % outdir, sep='\t', mode='a',
-              index=True, header=True if fold_num == 0 else False)
+                 index=x_test.index)\
+      .to_csv("%s/test_predictions.txt" % outdir, sep='\t', mode='a',
+              index=True, header=True if fold == 0 else False)
     # Annotate features
     pred_features = sorted(list(zip(estimator.feature_importances_,
-                                    X_train.columns[d[n_features][4]])),
+                                    x_train.columns[d[n_features][4]])),
                            reverse=True)
-    extract_feature_annotation(pred_features, outdir, fold_num, gtf_file)
+    extract_feature_annotation(pred_features, outdir, fold, gtf_file)
     # Save output data
     output = dict()
     output['n_max'] = n_features_max
@@ -178,13 +191,18 @@ def rf_run(X_train, X_test, Y_train, Y_test, fold_num, outdir,
     output['train_nmi_all_features'] = pfirst[1]
     output['train_acc_all_features'] = pfirst[2]
     output['train_roc_all_features'] = pfirst[3]
-    output['train_nmi'] = dRFEtools.oob_score_nmi(estimator, Y_train)
-    output['train_acc'] = dRFEtools.oob_score_accuracy(estimator, Y_train)
-    output['train_roc'] = dRFEtools.oob_score_roc(estimator, Y_train)
-    output['test_acc'] = accuracy_score(Y_test, labels_pred)
-    output['test_nmi'] = nmi(Y_test, labels_pred,
-                                   average_method="arithmetic")
-    output['test_roc'] = roc_auc_score(Y_test, labels_pred, **kwargs)
+    output['train_nmi'] = dRFEtools.dev_score_nmi(estimator,
+                                                  x_train.values[:,d[n_features][4]],
+                                                  y_train)
+    output['train_acc'] = dRFEtools.dev_score_accuracy(estimator,
+                                                       x_train.values[:,d[n_features][4]],
+                                                       y_train)
+    output['train_roc'] = dRFEtools.dev_score_roc(estimator,
+                                                  x_train.values[:,d[n_features][4]],
+                                                  y_train)
+    output['test_nmi'] = nmi(y_test, labels_pred, average_method="arithmetic")
+    output['test_acc'] = accuracy_score(y_test, labels_pred)
+    output['test_roc'] = roc_auc_score(y_test, labels_pred, **kwargs)
     output['test_acc_redundant'] = accuracy_score(Y_test, labels_pred_redundant)
     output['test_roc_redundant'] = roc_auc_score(Y_test, labels_pred_redundant,
                                                  **kwargs)
@@ -193,46 +211,31 @@ def rf_run(X_train, X_test, Y_train, Y_test, fold_num, outdir,
     return output, df_elim
 
 
-def load_data(fn):
-    pandas2ri.activate()
-    globalenv['fn'] = fn
-    r('''
-    suppressMessages({library(tidyverse)})
-    load(fn)
-    expr = as.data.frame(v$E)
-    model = data.frame(v$design)
-    genes = rownames(expr)
-    weights = as.data.frame(v$weights) %>% mutate(rowname=rownames(v$E)) %>%
-      column_to_rownames(var="rowname")
-    colnames(weights) = colnames(v$E)
-    ''')
-    return r['expr'].T, r['model'].MDD.astype("int64")
-
-
 def main_loop(args):
-    outdir = "rf"
+    outdir = "sgd"
     mkdir_p(outdir)
-    pandas2ri.activate()
-    cla = dRFEtools.RandomForestClassifier(n_estimators=100, oob_score=True,
-                                           n_jobs=-1, random_state=20211229)
+    ## Prep model
+    cla = dRFEtools.SGDClassifier(n_jobs=-1, loss="perceptron",
+                                  random_state=20211229)
     skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=20211004)
+    ## Load data
     X, Y = load_data(args.voom_file)
     skf.get_n_splits(X, Y)
+    ## Main analysis
+    frac = 0.30; step_size = 0.04; fold = 0
     fields = ['n_features_all_features', 'train_acc_all_features',
               'train_nmi_all_features', 'train_roc_all_features', 'n_max',
               'n_features', 'train_acc', 'train_nmi', 'train_roc', 'test_acc',
               'test_nmi', 'test_roc', 'n_redundant', 'test_acc_redundant',
               'test_nmi_redundant', 'test_roc_redundant']
-    optimize_rf(X, Y, cla, skf, outdir)
-    frac = 0.30; step_size = 0.04; fold = 0
     df_dict = pd.DataFrame()
     with open("%s/dRFEtools_10folds.txt" % (outdir), "w") as f:
         print("\t".join(["fold"] + fields), file=f, flush=True)
         for train_index, test_index in skf.split(X, Y):
             X_train, X_test = residualize(train_index, test_index)
             Y_train, Y_test = Y[train_index], Y[test_index]
-            o, df_elim = rf_run(X_train, X_test, Y_train, Y_test, fold, outdir,
-                                args.gtf_file, cla, frac, step_size)
+            o, df_elim = dev_run(X_train, X_test, Y_train, Y_test, fold, outdir,
+                                 args.gtf_file, cla, frac, step_size)
             df_dict = pd.concat([df_dict, df_elim], axis=0)
             print("\t".join([str(fold)] + [str(o[x]) for x in fields]),
                   flush=True, file=f)
