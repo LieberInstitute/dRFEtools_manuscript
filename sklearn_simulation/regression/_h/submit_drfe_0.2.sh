bee5da644f5b29ee12e9810e11bfe6857345090e
KrotosBenjamin <kj.benjamin90@gmail.com>
2022-06-28 15:06:39 -0400

    adding submission scripts for JHPCE

---
+++ sklearn_simulation/regression/_h/submit_drfe_0.2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#$ -cwd
+#$ -R y
+#$ -l mem_free=10.0G,h_vmem=10G,h_fsize=50G
+#$ -N 'regr_drfe_02_sklearn'
+#$ -o ./summary.out
+#$ -e ./summary.out
+#$ -m e -M jade.benjamin@libd.org
+
+echo "**** Job starts ****"
+date -Is
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+
+module load R/4.0.3
+module list
+
+echo "**** Run simulation ****"
+
+python ../_h/dynamicRFE_0.2.py
+
+echo "**** Job ends ****"
+date -Is
