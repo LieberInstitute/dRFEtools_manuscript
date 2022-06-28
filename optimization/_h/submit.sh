380350bd3fc5893184b388f4dec803e71da40365
KrotosBenjamin <kj.benjamin90@gmail.com>
2022-06-28 11:51:54 -0400

    updated for JHPCE submission

--- optimization/_h/submit.sh
+++ optimization/_h/submit.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
+#$ -cwd
+#$ -R y
+#$ -l mem_free=10.0G,h_vmem=10G,h_fsize=50G
+#$ -N 'drfetools_optimization'
+#$ -o ./summary.out
+#$ -e ./summary.out
+#$ -m e -M jade.benjamin@libd.org
 
 set -o errexit -o pipefail
 
 echo "**** Job starts ****"
 date -Is
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+
+module load pandoc
+module load R/4.0.3
+module list
 
 echo "**** Run jupyter notebook ****"
 
